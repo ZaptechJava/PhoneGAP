2bf060ee18ef3be9035104dce0448a59727f8cd7
Anuj.T <anuj@zapserver.com>
2014-11-12 19:16:04 +0530

    commit ignoring files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/anuj/PHONEGAP/Project/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/anuj/PHONEGAP/Project/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/anuj/PHONEGAP/Project/platforms/android/ant-build/AndroidManifest.xml : \
-/home/anuj/PHONEGAP/Project/platforms/android/AndroidManifest.xml \
