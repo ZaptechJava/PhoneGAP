bd01e6c50873c65248ced8b77cc3fb1bcbedbb83
Anuj.T <anuj@zapserver.com>
2014-11-21 10:45:35 +0530

    admob integrated,login error resolved,logout button issue solved

--- www/js/index.js
+++ www/js/index.js
@@ -48,10 +48,13 @@ var app = {
     }
 
 };
+
+//============================================LOGIN FUNCTION HERE========================================
 function login(){
-         
-    var name=document.getElementById('UserName').value;
-    var password=document.getElementById('password').value;
+    var name=$("#UserName").val();
+    var password=$("#password").val();
+    $('#UserName').val('');
+    $('#password').val('');
 
      if( name != "" || password != "")
      {
@@ -60,15 +63,105 @@ function login(){
             window.location = "Home.html";
             }
             else{
-            alert("Invalid Password");    
+            alert("Invalid Password");
+            password="";
             }     
          }
          else{
             alert("Invalid Username");
+            name="";
          }
     }
     else{
         alert("Username/Password can't be empty.")
     }
     //alert(name +" "+password);
+};
+
+//===========================To handle submit other than submit button.=================================
+$('#userform').submit(function(){
+    //$('#userform')[0].reset();
+    //$('#UserName').val(null);
+    //$('#password').val(null);
+    //alert("resetform");
+    return false;
+});
+
+//=====================================Ad Mob code here.=================================================
+function onDocLoad() {
+    if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+            document.addEventListener('deviceready', initApp, false);
+    }
+    else{
+        initApp();
+    }
 }
+    
+function initApp(){
+    initAd();
+    // display the banner at startup
+    window.plugins.AdMob.createBannerView();
+}
+    
+function initAd(){
+    if ( window.plugins && window.plugins.AdMob ) {
+        var ad_units = {
+            ios : {
+                banner: 'ca-app-pub-6869992474017983/4806197152',
+                interstitial: 'ca-app-pub-6869992474017983/7563979554'
+            },
+            android : {
+                banner: 'ca-app-pub-6869992474017983/9375997553',
+                interstitial: 'ca-app-pub-6869992474017983/1657046752'
+            },
+            wp8 : {
+                banner: 'ca-app-pub-6869992474017983/8878394753',
+                interstitial: 'ca-app-pub-6869992474017983/1355127956'
+            }
+        };
+        var admobid = "";
+        if( /(android)/i.test(navigator.userAgent) ) {
+            admobid = ad_units.android;
+        }
+        else if(/(iphone|ipad)/i.test(navigator.userAgent)) {
+            admobid = ad_units.ios;
+        }
+        else {
+            admobid = ad_units.wp8;
+        }
+
+        window.plugins.AdMob.setOptions( {
+            publisherId: admobid.banner,
+            interstitialAdId: admobid.interstitial,
+            bannerAtTop: false, // set to true, to put banner at top
+            overlap: false, // set to true, to allow banner overlap webview
+            offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+            isTesting: false, // receiving test ad
+            autoShow: true // auto show interstitial ad when loaded
+        });
+
+        registerAdEvents();
+        
+    }
+    else {
+            alert( 'admob plugin not ready' );
+    }
+}
+    
+// optional, in case respond to events
+function registerAdEvents() {
+    document.addEventListener('onReceiveAd', function(){});
+    document.addEventListener('onFailedToReceiveAd', function(data){});
+    document.addEventListener('onPresentAd', function(){});
+    document.addEventListener('onDismissAd', function(){ });
+    document.addEventListener('onLeaveToAd', function(){ });
+    document.addEventListener('onReceiveInterstitialAd', function(){ });
+    document.addEventListener('onPresentInterstitialAd', function(){ });
+    document.addEventListener('onDismissInterstitialAd', function(){ });
+}
+    
+function onResize() {
+    var msg = 'web view: ' + window.innerWidth + ' x ' + window.innerHeight;
+    document.getElementById('sizeinfo').innerHTML = msg;
+}
+//==========================================================================    
